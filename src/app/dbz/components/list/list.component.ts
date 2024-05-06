c2d53bc5a047174a5d1d6be4f755091028b59bd0
karique02 <kariquekeiter@gmail.com>
2024-05-06 11:27:18 -0500

    docs creado

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  @Input()
+  public characterList: Character[] = []
+
+  onDeleteCharacter(id: string | undefined) {
+    this.onDelete.emit(id);
+  }
+}
